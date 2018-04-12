e04556dbc12f109064a95f7ba67d76399aa4c2ba
Rob Harrington <oeoeaio@gmail.com>
2018-04-12 17:23:01 +1000

    Clean up comments and unused code

--- static/src/js/load_button.js
+++ static/src/js/load_button.js
@@ -4,21 +4,6 @@ odoo.define('pos.preorder.load_button', function (require) {
     var screens = require('point_of_sale.screens');
     var gui = require('point_of_sale.gui');
 
-
-    // odoo.define('pos.preorders.button', function (require) {
-    //     "use strict";
-    //     var screens = require('point_of_sale.screens');
-    //
-    //     screens.NumpadWidget.include({
-    //         renderElement: function () {
-    //             this._super();
-    //             $('.load-preorder').on('click', function () {
-    //                 self.gui.show_screen('preorders');
-    //             });
-    //         }
-    //     });
-    // });
-
     var LoadPreorderButton = screens.ActionButtonWidget.extend({
         template: 'LoadPreorderButton',
         button_click: function(){
@@ -33,5 +18,4 @@ odoo.define('pos.preorder.load_button', function (require) {
             return true; // TODO maybe add a config variable for this?
         },
     });
-
 });
